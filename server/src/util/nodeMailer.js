05cf0aae2c2d3fc755b0ba763ec7b4b83d6f7174
ImAltay <Bilman2705euw@gmail.com>
2024-07-18 20:03:21 +0200

    Function to send emails has been created

---
+++ server/src/util/nodeMailer.js
@@ -0,0 +1,39 @@
+const nodeMailer = require("nodemailer");
+import dotenv from "dotenv";
+import { logInfo, logError } from "./logging.js";
+
+// sentRecommendedBookEmail function is used to send an email with parameters from, to, subject, text, html
+
+dotenv.config();
+
+const transporter = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.HOST_EMAIL,
+    pass: process.env.HOST_EMAIL_PASSWORD,
+  },
+});
+
+export const sentRecommendedBookEmail = async (
+  from,
+  to,
+  subject,
+  text,
+  html,
+) => {
+  try {
+    const info = await transporter.sendMail({
+      from: from, // sender address
+      to: to, // list of receivers
+      subject: subject, // Subject line
+      text: text, // plain text body
+      html: html, // html body
+    });
+    logInfo("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  } catch (error) {
+    logError("Error sending email: %s", error);
+  }
+};
