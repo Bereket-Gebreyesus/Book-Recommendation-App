60a80156560abb43ade3e452cf6a3004589ca188
Ben Gumaan <ben.gumaan@gmail.com>
2024-07-19 16:39:31 +0200

    Modified User, Book, and Tag files and fixed some potential errors

--- server/src/models/Tag.js
+++ server/src/models/Tag.js
@@ -1,9 +1,18 @@
-const mongoose = import("mongoose");
+import mongoose from "mongoose";
 
-const tagSchema = new mongoose.Schema({
-  name: String,
-});
+const tagSchema = new mongoose.Schema(
+  {
+    tagId: { type: String, required: true, unique: true },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true, // Remove extra spaces around the tag name
+    },
+  },
+  { timestamps: true },
+); // Mongoose will automatically manage `createdAt` and `updatedAt`
 
-const Tag = mongoose.model("tags", tagSchema);
+const Tag = mongoose.model("Tag", tagSchema);
 
 export default Tag;
