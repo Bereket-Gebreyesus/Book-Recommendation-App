93e8551ef414a0808c27014a53a5fe006b4ef9fe
xrustik666 <kilorust@gmail.com>
2024-07-19 05:35:02 +0200

    scheduleEmailNotificationParams.js added

---
+++ server/src/util/notificator/scheduleEmailNotificationParams.js
@@ -0,0 +1,9 @@
+const emailNotificationParams = {
+  scheduleTime: "11 5 * * *",
+  from: "Books recommendations mailer",
+  subscribers: ["zubatkinnoname@yandex.com"],
+  subject: "Book recommendations",
+  htmlContent: "This is book recommendations for you",
+};
+
+export default emailNotificationParams;
