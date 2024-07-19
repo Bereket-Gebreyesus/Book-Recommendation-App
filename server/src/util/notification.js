b5e542bc362d175a40f3f57eea82ed58276626cf
xrustik666 <kilorust@gmail.com>
2024-07-19 02:43:16 +0200

    notification function changed according to mailer function

--- server/src/util/notification.js
+++ server/src/util/notification.js
@@ -1,38 +1,10 @@
-import nodemailer from "nodemailer"; // library for sending emails
 import cron from "node-cron"; // utility to run scheduled tasks
-import dotenv from "dotenv";
-import { logInfo, logError } from "./logging.js";
-
-dotenv.config();
-
-//! Creating transporter for sending mail using nodemailer (just to test how notification works)
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-//! Sending emails using nodemailer (just to test how notification works)
-async function sendEmail(subscriberEmail, subject, htmlContent) {
-  try {
-    const info = await transporter.sendMail({
-      to: subscriberEmail,
-      subject: subject,
-      html: htmlContent,
-    });
-
-    logInfo("Email sent: " + info.response);
-  } catch (err) {
-    logError("Error sending email:", err);
-  }
-}
+import { sentRecommendedBookEmail } from "./nodeMailer.js";
 
 // Setting a schedule for mailing using node-cron
 function scheduleNotificator() {
   cron.schedule(
-    "32 14 * * 4",
+    "0 9 * * 5",
     () => {
       // This is an example list of subscribers. Later the data will be taken from db
       const subscribers = ["zubatkinnoname@yandex.ru"];
@@ -42,7 +14,12 @@ function scheduleNotificator() {
         // This is an example content. Later we will work out how recomendations are calculated
         const htmlContent = `<p>This is <b>individual recommendations content</b> for ${subscriber}:</p>`;
 
-        sendEmail(subscriber, subject, htmlContent);
+        sentRecommendedBookEmail(
+          "Book recomendations app",
+          subscriber,
+          subject,
+          htmlContent,
+        );
       });
     },
     {
