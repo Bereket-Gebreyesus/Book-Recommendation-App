30a169d7bda8b085a46b84fc1c2f9639e4896638
Be-Ge <bereketgebreyesusnl@gmail.com>
2024-07-28 16:23:56 +0200

    structure code and creat reusable components

--- client/src/pages/Book/BookDetail.jsx
+++ client/src/pages/Book/BookDetail.jsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import PropTypes from "prop-types";
 import useFetch from "../../hooks/useFetch";
 import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
-import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
-import StarRating from "../../components/StarRating";
+import BookImage from "../../components/Book/BookImage";
+import BookInfo from "../../components/Book/BookInfo";
+import Reviews from "../../components/Review/Reviews";
 import RatingStats from "../../components/RatingStats";
-import Pagination from "../../components/Pagination";
 
 const BookDetail = () => {
   const { id } = useParams();
@@ -86,7 +83,6 @@ const BookDetail = () => {
   } else if (!book) {
     content = <p>No book details available.</p>;
   } else {
-    // Safe handling for undefined or null averageRating
     const averageRating = book.averageRating
       ? book.averageRating.toFixed(1)
       : "N/A";
@@ -98,78 +94,11 @@ const BookDetail = () => {
     content = (
       <Container>
         <Row className="align-items-start mt-5">
-          <Col md={4} className="mb-4" style={{ position: "relative" }}>
-            {book.image ? (
-              <img
-                src={book.image}
-                alt={book.title}
-                style={{
-                  maxWidth: "100%",
-                  height: "500px",
-                  objectFit: "cover",
-                  display: "flex",
-                  borderRadius: "8px",
-                  zIndex: 10,
-                  transition: "transform 0.3s ease-in-out",
-                }}
-                onMouseOver={(e) =>
-                  (e.currentTarget.style.transform = "scale(1.05)")
-                }
-                onMouseOut={(e) =>
-                  (e.currentTarget.style.transform = "scale(1)")
-                }
-              />
-            ) : (
-              <div
-                style={{
-                  maxWidth: "100%",
-                  height: "500px",
-                  backgroundColor: "#ccc",
-                  borderRadius: "8px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  fontSize: "1.5rem",
-                  color: "#666",
-                  transition: "transform 0.3s ease-in-out",
-                }}
-                onMouseOver={(e) =>
-                  (e.currentTarget.style.transform = "scale(1.05)")
-                }
-                onMouseOut={(e) =>
-                  (e.currentTarget.style.transform = "scale(1)")
-                }
-              >
-                No Image Available
-              </div>
-            )}
+          <Col md={4} className="mb-4">
+            <BookImage image={book.image} title={book.title} />
           </Col>
           <Col md={8}>
-            <h1 style={{ fontFamily: "'Georgia', serif", fontSize: "2.5rem" }}>
-              {book.title}
-            </h1>
-            <h4 style={{ color: "#888" }}>By {book.authors.join(", ")}</h4>
-            {averageRating !== "N/A" && (
-              <p>
-                <strong>Average Rating:</strong> {averageRating}{" "}
-                <StarRating rating={parseFloat(averageRating)} />
-              </p>
-            )}
-            <p>{book.description}</p>
-            <p>
-              <strong>ISBN:</strong> {book.isbn}
-            </p>
-            <p>
-              <strong>Published Date:</strong>{" "}
-              {new Date(book.publishedDate).toDateString()}
-            </p>
-            <p>
-              <strong>Publisher:</strong> {book.publisher}
-            </p>
-            <div>
-              <strong>Tags:</strong>{" "}
-              {book.tags.map((tagId) => tags[tagId] || "No tags").join(", ")}
-            </div>
+            <BookInfo book={book} tags={tags} />
           </Col>
         </Row>
         {averageRating !== "N/A" && (
@@ -179,23 +108,9 @@ const BookDetail = () => {
             totalReviews={book.reviews.length}
           />
         )}
-        {currentReviews.length > 0 ? (
-          <>
-            <h3>Reviews</h3>
-            <ListGroup>
-              {currentReviews.map((review) => (
-                <ReviewItem
-                  key={review._id}
-                  review={review}
-                  reviewer={reviewers[review.ownerId]}
-                />
-              ))}
-            </ListGroup>
-          </>
-        ) : (
-          <p>No reviews yet</p>
-        )}
-        <Pagination
+        <Reviews
+          reviews={currentReviews}
+          reviewers={reviewers}
           totalPages={totalPages}
           currentPage={currentPage}
           onPageChange={handlePageChange}
@@ -205,73 +120,6 @@ const BookDetail = () => {
   }
 
   return <div>{content}</div>;
-};
-
-const ReviewItem = ({ review, reviewer }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleReadMore = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const displayText = isExpanded
-    ? review.text
-    : `${review.text.substring(0, 200)}...`;
-
-  return (
-    <ListGroup.Item>
-      <div className="d-flex align-items-center mb-2">
-        {reviewer?.profileImage && (
-          <Image
-            src={reviewer.profileImage}
-            roundedCircle
-            style={{ width: "50px", height: "50px", marginRight: "10px" }}
-          />
-        )}
-        <div>
-          <strong>{reviewer?.name || "Unknown"}</strong>
-          <div>
-            <StarRating rating={review.rating} />
-          </div>
-          <small>{new Date(review.created_at).toLocaleDateString()}</small>
-        </div>
-      </div>
-      <p>{displayText}</p>
-      {review.text.length > 100 && (
-        <Button
-          variant="link"
-          onClick={toggleReadMore}
-          style={{
-            color: "#00FF00",
-          }}
-        >
-          {isExpanded ? "Read Less" : "Read More"}
-        </Button>
-      )}
-    </ListGroup.Item>
-  );
-};
-
-BookDetail.propTypes = {
-  id: PropTypes.string,
-  reviewsPerPage: PropTypes.number,
-};
-
-BookDetail.defaultProps = {
-  reviewsPerPage: 5,
-};
-
-ReviewItem.propTypes = {
-  review: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    created_at: PropTypes.string.isRequired,
-  }).isRequired,
-  reviewer: PropTypes.shape({
-    name: PropTypes.string,
-    profileImage: PropTypes.string,
-  }),
 };
 
 export default BookDetail;
