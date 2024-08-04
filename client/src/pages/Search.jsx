2467498144937d848c869bd121f04eee8e293c6c
xrustik666 <kilorust@gmail.com>
2024-08-04 02:17:33 +0200

    results displayed well

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 const Search = () => {
   const [books, setBooks] = useState([]);
+  const [tags, setTags] = useState({});
   const location = useLocation();
   const query = new URLSearchParams(location.search).get("query");
 
   const onReceived = (data) => {
     setBooks(data.books);
+    const tagIds = data.books.flatMap((book) => book.tags);
+    if (tagIds.length > 0) {
+      performFetchTags(tagIds);
+    }
   };
 
   const { isLoading, error, performFetch, cancelFetch } = useFetch(
     `/books/search?query=${query}`,
     onReceived,
   );
+
+  const { performFetch: performFetchTags } = useFetch("/tags", (response) => {
+    const tagMap = response.result.reduce((acc, tag) => {
+      acc[tag._id] = tag.name;
+      return acc;
+    }, {});
+    setTags(tagMap);
+  });
 
   useEffect(() => {
     performFetch();
@@ -28,22 +41,31 @@ const Search = () => {
 
   return (
     <div>
-      <h1>Search Results</h1>
+      <h4 className="mb-4">Search Results</h4>
       {books.length > 0 ? (
-        <ul>
+        <ul className="list-unstyled">
           {books.map((book) => (
-            <li key={book._id}>
-              <h2>{book.title}</h2>
-              <p>
-                <strong>Author:</strong> {book.author}
-              </p>
-              <p>
-                <strong>Description:</strong> {book.description.slice(0, 100)}
-                ...
-              </p>
-              <p>
-                <strong>Tags:</strong> {book.tags.join(", ")}
-              </p>
+            <li key={book._id} className="border p-3 mb-4 shadow-sm">
+              <Link to={`/books/${book._id}`} className="text-decoration-none">
+                <div>
+                  <h3 className="h5 text-dark fw-bold">{book.title}</h3>
+                  <p className="mb-1 text-muted ">
+                    by {book.authors || "Author is unknown"}
+                  </p>
+                  <p className="text-muted mb-1">
+                    Description:{" "}
+                    {book.description
+                      ? `${book.description.slice(0, 100)}...`
+                      : "No description"}
+                  </p>
+                  <p className="text-muted mb-1">
+                    Tags:{" "}
+                    {book.tags && book.tags.length > 0
+                      ? book.tags.map((tagId) => tags[tagId]).join(", ")
+                      : "No tags"}
+                  </p>
+                </div>
+              </Link>
             </li>
           ))}
         </ul>
