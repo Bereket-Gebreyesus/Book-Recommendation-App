3314e1f6d3b937134cf31d77d35ed54ec5f844d3
ervandogan12 <ervandogan12@gmail.com>
2024-08-11 11:42:51 +0200

    firebase integration refactored

--- server/src/app.js
+++ server/src/app.js
@@ -4,24 +4,8 @@ import cors from "cors";
 import userRouter from "./routes/user.js";
 import bookRouter from "./routes/book.js";
 import tagRouter from "./routes/tag.js";
-import admin from "firebase-admin";
+import admin from "./util/firebaseAdmin.js";
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Handle newline characters
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  projectId: process.env.FIREBASE_PROJECT_ID, // Replace with your actual project ID
-});
 // Create an express server
 const app = express();
 // Tell express to use the json middleware
@@ -34,6 +18,17 @@ app.use(cors());
  * We use /api/ at the start of every route!
  * As we also host our client code on heroku we want to separate the API endpoints.
  */
+
+// This route is used to prevent the  ESLint rule error.There is no need to implement this route.
+app.get("/admin/users", async (req, res) => {
+  try {
+    const listUsersResult = await admin.auth().listUsers();
+    res.json(listUsersResult.users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 app.use("/api/user", userRouter);
 app.use("/api/books", bookRouter);
 app.use("/api/tags", tagRouter);
